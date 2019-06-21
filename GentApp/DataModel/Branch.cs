5fd3605f60e0d2ca89d455a43b1312a8fdd47ef0
NathanWesterlinck <nathan.westerlinck.y7716@student.hogent.be>
2019-06-21 16:06:13 +0200

    Added models for company, branch and user

---
+++ GentApp/DataModel/Branch.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GentApp.DataModel
+{
+    class Branch
+    {
+        private string _name;
+        private BranchType type;
+        private string _companyId;
+        private string _address;
+
+        public Branch()
+        {
+        }
+
+        public Branch(string name, BranchType type, string companyId, string address)
+        {
+            _name = name;
+            this.type = type;
+            _companyId = companyId;
+            _address = address;
+        }
+
+        public string Name { get => _name; set => _name = value; }
+        public string CompanyId { get => _companyId; set => _companyId = value; }
+        public string Address { get => _address; set => _address = value; }
+        internal BranchType Type { get => type; set => type = value; }
+    }
+}
