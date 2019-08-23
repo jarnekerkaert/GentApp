3eccd03af9fdf45143ed81f8282abe20d5777cae
NathanWesterlinck <nathan.westerlinck.y7716@student.hogent.be>
2019-08-23 21:11:38 +0200

    Added functionality to the autosuggestbox for filtering on branches

--- GentApp/Views/HomePage.xaml.cs
+++ GentApp/Views/HomePage.xaml.cs
@@ -1,5 +1,9 @@
-﻿using GentApp.DataModel;
+﻿using GalaSoft.MvvmLight.Ioc;
+using GentApp.DataModel;
+using GentApp.ViewModels;
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using Windows.UI.Xaml.Controls;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
@@ -12,6 +16,43 @@ namespace GentApp.Views {
 		public HomePage() {
 			InitializeComponent();
 			companyTypeComboBox.ItemsSource = Enum.GetValues(typeof(BranchType));
+			//Branches = new List<Branch>(SimpleIoc.Default.GetInstance<BranchesViewModel>().Branches);
+			//autoSuggestBoxBranch.ItemsSource = SimpleIoc.Default.GetInstance<BranchesViewModel>().Branches;
+			//autoSuggestBoxBranch.ItemsSource = Branches;
 		}
+
+		//public List<Branch> Branches { get; set; }
+		//public List<string> BranchNames { get; set; }
+
+		private void AutoSuggestBoxBranch_TextChanged(AutoSuggestBox sender, AutoSuggestBoxTextChangedEventArgs args)
+		{
+			if (args.CheckCurrent())
+			{
+				var search_term = autoSuggestBoxBranch.Text;
+				if (SimpleIoc.Default.GetInstance<BranchesViewModel>().Branches != null)
+				{
+					autoSuggestBoxBranch.ItemsSource = SimpleIoc.Default.GetInstance<BranchesViewModel>().Branches;
+					var results = SimpleIoc.Default.GetInstance<BranchesViewModel>().Branches.Where(b => b.Name.Contains(search_term)).ToList();
+					//autoSuggestBoxBranch.ItemsSource = results;
+					List<string> name_results = new List<string>();
+					results.ForEach(b => name_results.Add(b.Name));
+					autoSuggestBoxBranch.ItemsSource = name_results;
+					HomeFeedGrid.ItemsSource = results;
+				}
+			}
+
+		}
+
+		private void AutoSuggestBoxBranch_QuerySubmitted(AutoSuggestBox sender, AutoSuggestBoxQuerySubmittedEventArgs args)
+		{
+			var search_term = args.QueryText;
+			if (SimpleIoc.Default.GetInstance<BranchesViewModel>().Branches != null)
+			{
+				var results = SimpleIoc.Default.GetInstance<BranchesViewModel>().Branches.Where(b => b.Name.Contains(search_term)).ToList();
+				//autoSuggestBoxBranch.ItemsSource = results;
+				HomeFeedGrid.ItemsSource = results;
+			}
+		}
+
 	}
 }
